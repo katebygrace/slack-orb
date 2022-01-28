0f20c4f1063f842a81c34aa218ae6ba44029cdbd
katebygrace <63807774+katebygrace@users.noreply.github.com>
2022-01-28 11:40:02 -0500

    Update notify.sh

--- src/scripts/notify.sh
+++ src/scripts/notify.sh
@@ -46,12 +46,13 @@ PostToSlack() {
         if [ -n "${SLACK_PARAM_DEBUG:-}" ]; then
             echo "The response from the API call to slack is : $SLACK_SENT_RESPONSE"
         fi        
-        SLACK_ERROR_MSG=$(echo "$SLACK_SENT_RESPONSE" | jq '.error')
-        if [ -z "$SLACK_ERROR_MSG" ]; then
+        SLACK_ERROR_OK=$(echo "$SLACK_SENT_RESPONSE" | jq '.ok' )
+        SLACK_ERROR_RESPONSE=$(echo "$SLACK_SENT_RESPONSE" | jq '.error' )
+        if [ "$SLACK_ERROR_OK" = true ]; then
             echo "The Slack API returned no errors."
         else    
             echo "Slack API returned an error message:"
-            echo "$SLACK_ERROR_MSG"
+            echo "$SLACK_ERROR_RESPONSE"
             echo
             echo
             echo "View the Setup Guide: https://github.com/CircleCI-Public/slack-orb/wiki/Setup"
